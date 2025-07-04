01b429626043098071edb663bf905a7971b02fbe
farrelathalla <farrelxag@gmail.com>
2025-07-04 21:31:34 +0700

    feat: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,9 +34,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
-        <Navbar />
-        {children}
+      <body className={`${poppins.className}`}>
+        <div className="bg-gradient-to-bl from-[#060911] to-[#232630] flex flex-col min-h-screen h-full">
+          <Navbar />
+          <main className="flex-grow ">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const page = () => {
   return (
-    <main className="h-screen bg-gradient-to-bl from-[#060911] to-[#232630]"></main>
+    <div className="p-32">
+      <h1>dasdsd</h1>
+    </div>
   );
 };
 
